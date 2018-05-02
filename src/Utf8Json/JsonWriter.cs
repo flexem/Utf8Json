6bdb469921eb9b742d5a97fa0c8e78a8e7ef4a6e
Fryderyk Huang <fryderyk.huang@gmail.com>
2018-05-02 16:01:25 +0800

    escape unicode surrogate character

--- src/Utf8Json/JsonWriter.cs
+++ src/Utf8Json/JsonWriter.cs
@@ -380,165 +380,180 @@ namespace Utf8Json
             {
                 byte escapeChar = default(byte);
                 var c = value[i];
-                char unicode2 = '\0';
-                char unicode3 = '\0';
+                byte unicode0 = 48;
+                byte unicode1 = 48;
+                byte unicode2 = 48;
+                byte unicode3 = 48;
                 switch (c)
                 {
                     case '"':
-                        escapeChar = (byte)'"';
+                        escapeChar = (byte) '"';
                         break;
                     case '\\':
-                        escapeChar = (byte)'\\';
+                        escapeChar = (byte) '\\';
                         break;
                     case '\b':
-                        escapeChar = (byte)'b';
+                        escapeChar = (byte) 'b';
                         break;
                     case '\f':
-                        escapeChar = (byte)'f';
+                        escapeChar = (byte) 'f';
                         break;
                     case '\n':
-                        escapeChar = (byte)'n';
+                        escapeChar = (byte) 'n';
                         break;
                     case '\r':
-                        escapeChar = (byte)'r';
+                        escapeChar = (byte) 'r';
                         break;
                     case '\t':
-                        escapeChar = (byte)'t';
+                        escapeChar = (byte) 't';
                         break;
                     // use switch jumptable
-                    case (char)0:
-                    case (char)1:
-                    case (char)2:
-                    case (char)3:
-                    case (char)4:
-                    case (char)5:
-                    case (char)6:
-                    case (char)7:
-                    case (char)11:
-                    case (char)14:
-                    case (char)15:
-                    case (char)16:
-                    case (char)17:
-                    case (char)18:
-                    case (char)19:
-                    case (char)20:
-                    case (char)21:
-                    case (char)22:
-                    case (char)23:
-                    case (char)24:
-                    case (char)25:
-                    case (char)26:
-                    case (char)27:
-                    case (char)28:
-                    case (char)29:
-                    case (char)30:
-                    case (char)31:
-                    case (char)127:
-                    case (char)128:
-                    case (char)129:
-                    case (char)131:
-                    case (char)132:
-                    case (char)133:
-                    case (char)134:
-                    case (char)135:
-                    case (char)136:
-                    case (char)137:
-                    case (char)138:
-                    case (char)139:
-                    case (char)140:
-                    case (char)141:
-                    case (char)142:
-                    case (char)143:
-                    case (char)144:
-                    case (char)145:
-                    case (char)146:
-                    case (char)147:
-                    case (char)148:
-                    case (char)149:
-                    case (char)150:
-                    case (char)151:
-                    case (char)152:
-                    case (char)153:
-                    case (char)154:
-                    case (char)155:
-                    case (char)156:
-                    case (char)157:
-                    case (char)158:
-                    case (char)159:
-                        escapeChar = (byte)'u';
-                        unicode2 = (char) (c >> 4);
-                        unicode2 = unicode2 < 10 ? (char) (unicode2 + 48) : (char) (unicode2 + 55);
-                        unicode3 = (char) (c & 4);
-                        unicode3 = unicode3 < 10 ? (char)(unicode3 + 48) : (char)(unicode3 + 55);
+                    case (char) 0:
+                    case (char) 1:
+                    case (char) 2:
+                    case (char) 3:
+                    case (char) 4:
+                    case (char) 5:
+                    case (char) 6:
+                    case (char) 7:
+                    case (char) 11:
+                    case (char) 14:
+                    case (char) 15:
+                    case (char) 16:
+                    case (char) 17:
+                    case (char) 18:
+                    case (char) 19:
+                    case (char) 20:
+                    case (char) 21:
+                    case (char) 22:
+                    case (char) 23:
+                    case (char) 24:
+                    case (char) 25:
+                    case (char) 26:
+                    case (char) 27:
+                    case (char) 28:
+                    case (char) 29:
+                    case (char) 30:
+                    case (char) 31:
+                    case (char) 127:
+                    case (char) 128:
+                    case (char) 129:
+                    case (char) 131:
+                    case (char) 132:
+                    case (char) 133:
+                    case (char) 134:
+                    case (char) 135:
+                    case (char) 136:
+                    case (char) 137:
+                    case (char) 138:
+                    case (char) 139:
+                    case (char) 140:
+                    case (char) 141:
+                    case (char) 142:
+                    case (char) 143:
+                    case (char) 144:
+                    case (char) 145:
+                    case (char) 146:
+                    case (char) 147:
+                    case (char) 148:
+                    case (char) 149:
+                    case (char) 150:
+                    case (char) 151:
+                    case (char) 152:
+                    case (char) 153:
+                    case (char) 154:
+                    case (char) 155:
+                    case (char) 156:
+                    case (char) 157:
+                    case (char) 158:
+                    case (char) 159:
+                        escapeChar = (byte) 'u';
+                        unicode2 = (byte) (c >> 4);
+                        unicode2 = (byte) (unicode2 < 10 ? unicode2 + 48 : unicode2 + 55);
+                        unicode3 = (byte) (c & 0xF);
+                        unicode3 = (byte) (unicode3 < 10 ? unicode3 + 48 : unicode3 + 55);
                         max += 4;
                         break;
 
-
-
-                    case (char)32:
-                    case (char)33:
-                    case (char)35:
-                    case (char)36:
-                    case (char)37:
-                    case (char)38:
-                    case (char)39:
-                    case (char)40:
-                    case (char)41:
-                    case (char)42:
-                    case (char)43:
-                    case (char)44:
-                    case (char)45:
-                    case (char)46:
-                    case (char)47:
-                    case (char)48:
-                    case (char)49:
-                    case (char)50:
-                    case (char)51:
-                    case (char)52:
-                    case (char)53:
-                    case (char)54:
-                    case (char)55:
-                    case (char)56:
-                    case (char)57:
-                    case (char)58:
-                    case (char)59:
-                    case (char)60:
-                    case (char)61:
-                    case (char)62:
-                    case (char)63:
-                    case (char)64:
-                    case (char)65:
-                    case (char)66:
-                    case (char)67:
-                    case (char)68:
-                    case (char)69:
-                    case (char)70:
-                    case (char)71:
-                    case (char)72:
-                    case (char)73:
-                    case (char)74:
-                    case (char)75:
-                    case (char)76:
-                    case (char)77:
-                    case (char)78:
-                    case (char)79:
-                    case (char)80:
-                    case (char)81:
-                    case (char)82:
-                    case (char)83:
-                    case (char)84:
-                    case (char)85:
-                    case (char)86:
-                    case (char)87:
-                    case (char)88:
-                    case (char)89:
-                    case (char)90:
-                    case (char)91:
+                    case (char) 32:
+                    case (char) 33:
+                    case (char) 35:
+                    case (char) 36:
+                    case (char) 37:
+                    case (char) 38:
+                    case (char) 39:
+                    case (char) 40:
+                    case (char) 41:
+                    case (char) 42:
+                    case (char) 43:
+                    case (char) 44:
+                    case (char) 45:
+                    case (char) 46:
+                    case (char) 47:
+                    case (char) 48:
+                    case (char) 49:
+                    case (char) 50:
+                    case (char) 51:
+                    case (char) 52:
+                    case (char) 53:
+                    case (char) 54:
+                    case (char) 55:
+                    case (char) 56:
+                    case (char) 57:
+                    case (char) 58:
+                    case (char) 59:
+                    case (char) 60:
+                    case (char) 61:
+                    case (char) 62:
+                    case (char) 63:
+                    case (char) 64:
+                    case (char) 65:
+                    case (char) 66:
+                    case (char) 67:
+                    case (char) 68:
+                    case (char) 69:
+                    case (char) 70:
+                    case (char) 71:
+                    case (char) 72:
+                    case (char) 73:
+                    case (char) 74:
+                    case (char) 75:
+                    case (char) 76:
+                    case (char) 77:
+                    case (char) 78:
+                    case (char) 79:
+                    case (char) 80:
+                    case (char) 81:
+                    case (char) 82:
+                    case (char) 83:
+                    case (char) 84:
+                    case (char) 85:
+                    case (char) 86:
+                    case (char) 87:
+                    case (char) 88:
+                    case (char) 89:
+                    case (char) 90:
+                    case (char) 91:
                     default:
-                        continue;
+                        if (!char.IsSurrogate(c)) continue;
+
+                        escapeChar = (byte) 'u';
+                        unicode0 = (byte) ((c >> 12) & 0xF);
+                        unicode0 = (byte) (unicode0 < 10 ? unicode0 + 48 : unicode0 + 55);
+
+                        unicode1 = (byte) ((c >> 8) & 0xF);
+                        unicode1 = (byte) (unicode1 < 10 ? unicode1 + 48 : unicode1 + 55);
+
+                        unicode2 = (byte) ((c >> 4) & 0xF);
+                        unicode2 = (byte) (unicode2 < 10 ? unicode2 + 48 : unicode2 + 55);
+
+                        unicode3 = (byte) (c & 0xF);
+                        unicode3 = (byte) (unicode3 < 10 ? unicode3 + 48 : unicode3 + 55);
+                        max += 4;
+                        break;
                 }
 
-                max += 2;
+                max += 1;
                 BinaryUtil.EnsureCapacity(ref buffer, startoffset, max); // check +escape capacity
 
                 offset += StringEncoding.UTF8.GetBytes(value, from, i - from, buffer, offset);
@@ -546,10 +561,10 @@ namespace Utf8Json
                 buffer[offset++] = (byte)'\\';
                 buffer[offset++] = escapeChar;
                 if (escapeChar != (byte) 'u') continue;
-                buffer[offset++] = (byte) '0';
-                buffer[offset++] = (byte) '0';
-                buffer[offset++] = (byte) unicode2;
-                buffer[offset++] = (byte) unicode3;
+                buffer[offset++] = unicode0;
+                buffer[offset++] = unicode1;
+                buffer[offset++] = unicode2;
+                buffer[offset++] = unicode3;
             }
 
             if (from != value.Length)
